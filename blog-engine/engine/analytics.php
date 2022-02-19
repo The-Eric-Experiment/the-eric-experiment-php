51fb3563df9440bc9245434f9c315c1897bd9dc2
Eric Mackrodt <ericmackrodt@gmail.com>
2022-02-19 11:47:14 +1100

    variant analytics

--- blog-engine/engine/analytics.php
+++ blog-engine/engine/analytics.php
@@ -2,9 +2,23 @@
 
 function getRemoteIP()
 {
-  return $_SERVER['HTTP_CF_CONNECTING_IP'] ??
-    $_SERVER['HTTP_X_FORWARDED_FOR'] ??
-    $_SERVER['REMOTE_ADDR'] ?? "";
+  if (array_key_exists('HTTP_X_REAL_IP', $_SERVER)) {
+    return $_SERVER['HTTP_X_REAL_IP'];
+  }
+
+  if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+    return $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+
+  if (array_key_exists('HTTP_CF_CONNECTING_IP', $_SERVER)) {
+    return $_SERVER['HTTP_CF_CONNECTING_IP'];
+  }
+
+  if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+    return $_SERVER['REMOTE_ADDR'];
+  }
+
+  return "";
 }
 
 function getUserAgent()
@@ -26,27 +40,22 @@ function getUrlPath()
     "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 }
 
-$ip = getRemoteIP();
-$userAgent = getUserAgent();
-$referrer = getReferer();
-$locationUrl = getUrlPath();
-$parsedUrl = parse_url($locationUrl);
-$documentPath = $parsedUrl["path"];
-$documentQuery = $parsedUrl["query"] ?? "";
-$documentHost = $parsedUrl["host"];
 
-if (strpos($documentHost, 'localhost') === false) {
-  $url = 'https://analytics.ericexperiment.com/api/event';
-
-  $data = '
+/**
+ * Tracks events
+ * 
+ * @param string $event_name The name of the event
+ * @param array|boolean $props The Props
+ * 
+ * @return void;
+ */
+function trackEvent(string $event_name, $props = false)
 {
-  "name":"pageview",
-  "url":"' . $locationUrl . '",
-  "domain":"ericexperiment.com",
-  "screen_width":1666,
-  "referrer": "' . $referrer . '"
-}
-';
+  $ip = getRemoteIP();
+  $userAgent = getUserAgent();
+  $referrer = getReferer();
+  $locationUrl = getUrlPath();
+  $event_url = 'https://analytics.ericexperiment.com/api/event';
 
   $additional_headers = array(
     "User-Agent: {$userAgent}",
@@ -54,11 +63,28 @@ if (strpos($documentHost, 'localhost') === false) {
     'Content-Type: application/json',
   );
 
-  $ch = curl_init($url);
+  $data = array(
+    "name" => $event_name,
+    "url" => $locationUrl,
+    "domain" => "ericexperiment.com",
+    "screen_width" => 1666,
+    "referrer" => $referrer
+  );
+
+  if ($props !== false) {
+    $data["props"] = json_encode($props);
+  }
+
+  $ch = curl_init($event_url);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers);
 
   curl_exec($ch);
+}
+
+if (strpos($documentHost, 'localhost') === false) {
+  trackEvent('pageview');
+  trackEvent('website_variant', array("mode" => "retro"));
 }
