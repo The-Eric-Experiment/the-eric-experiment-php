8b3cbabcb13d274bfeb036610be87801ad5655bd
Eric Mackrodt <ericmackrodt@gmail.com>
2022-09-15 17:48:48 -0700

    remove parsedown

--- blog-engine/engine/file-loader.php
+++ blog-engine/engine/file-loader.php
@@ -1,7 +1,5 @@
 <?php
 
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
 $config = require __DIR__.'/../config.php';
 
 define('BLOG_PAGE_PATH', __DIR__.'/../'.$config['pages-folder']);
@@ -29,28 +27,4 @@ function postExists($slug)
     }
 
     return true;
-}
-
-function loadPageMd($page)
-{
-    $filePath = BLOG_PAGE_PATH.'/'.$page.'.md';
-    $md = @file_get_contents($filePath);
-    if ($md === false) {
-        throw new PageNotFoundException('Page not found');
-    }
-
-    return YamlFrontMatter::parse($md);
-}
-
-function loadPostMd($slug)
-{
-    global $config;
-    $post_path = BLOG_POST_PATH."/{$slug}/";
-    $file_path = $post_path.$config['post-filename'];
-    $md = @file_get_contents($file_path);
-    if ($md === false) {
-        throw new PageNotFoundException('Post not found');
-    }
-
-    return ['object' => YamlFrontMatter::parse($md), 'post_path' => $post_path];
 }
--- blog-engine/index.php
+++ blog-engine/index.php
@@ -1,20 +1,18 @@
 <?php
+
 try {
-  require 'vendor/autoload.php';
-  require 'engine/helpers.php';
-  require 'engine/exceptions.php';
-  require 'engine/browser-selector.php';
-  require 'engine/db.php';
-  require 'engine/paginator.php';
-  // require 'engine/https-redirect.php';
-  require 'engine/variant-selector.php';
-  require 'engine/image-mapper.php';
-  require 'engine/file-loader.php';
-  require 'engine/parsers/gallery-parser.php';
-  require 'engine/templating.php';
-  require 'engine/parsers/markdown-parser.php';
-  require "engine/routes.php";
+    require 'vendor/autoload.php';
+    require 'engine/helpers.php';
+    require 'engine/exceptions.php';
+    require 'engine/browser-selector.php';
+    require 'engine/db.php';
+    require 'engine/paginator.php';
+    require 'engine/variant-selector.php';
+    require 'engine/image-mapper.php';
+    require 'engine/file-loader.php';
+    require 'engine/templating.php';
+    require 'engine/routes.php';
 } catch (Exception $e) {
-  echo var_dump($e);
-  echo var_dump($e->getTrace());
+    echo var_dump($e);
+    echo var_dump($e->getTrace());
 }
