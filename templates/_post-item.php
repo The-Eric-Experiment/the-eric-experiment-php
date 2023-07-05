55d883db98a4d30a8d07f1a8618f451ed8e796b7
Eric Mackrodt <ericmackrodt@gmail.com>
2023-07-05 04:03:24 -0700

    minor text fix

--- templates/_post-item.php
+++ templates/_post-item.php
@@ -2,9 +2,7 @@
 <a href="/post/<?=$slug?>">
     <img src="<?= $image ?>" alt="<?= $title ?>" width="80" border="0" align="<?= $image_side ?>">
 </a>
-<b>
-    <a href="/post/<?=$slug?>"><?= $this->text($title, []) ?></a>
-</b>&nbsp;-&nbsp;<?= $this->text($description, []) ?>
+<a href="/post/<?=$slug?>"><?= $this->text($title, ['bold' => true]) ?></a> - <?= $this->text($description, []) ?>
 <br clear="ALL">
 </p>
 
