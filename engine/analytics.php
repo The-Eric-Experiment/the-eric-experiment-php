d6a4337cc569b56d68fe8486347450db5714c7c8
Eric Mackrodt <ericmackrodt@gmail.com>
2023-12-18 11:42:52 +0000

    fix analytics probably

--- engine/analytics.php
+++ engine/analytics.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'headers.php';
-$documentHost = '';
 $analytics_service_url = getenv('ANALYTICS_SERVICE_URL', true) ?: getenv('ANALYTICS_SERVICE_URL');
 /**
  * Tracks events.
@@ -13,41 +12,48 @@ $analytics_service_url = getenv('ANALYTICS_SERVICE_URL', true) ?: getenv('ANALYT
  */
 function trackEvent(string $event_name, $props = false)
 {
-    $ip = getRemoteIP();
-    $userAgent = getUserAgent();
-    $referer = getReferer();
-    $locationUrl = getUrlPath();
-    $parsedUrl = parse_url($locationUrl);
-    $documentHost = $parsedUrl['host'];
-    $event_url = 'http://'.$analytics_service_url.'/api/event';
+  global $analytics_service_url;
+  $ip = getRemoteIP();
+  $userAgent = getUserAgent();
+  $referer = getReferer();
+  $locationUrl = getUrlPath();
+  $parsedUrl = parse_url($locationUrl);
+  $documentHost = $parsedUrl['host'];
 
-    $additional_headers = [
+  if (strpos($documentHost, 'localhost') !== false) {
+    return;
+  }
+
+  $event_url = 'http://' . $analytics_service_url . '/api/event';
+
+
+  $additional_headers = [
     "User-Agent: {$userAgent}",
     "HTTP_X_REAL_IP: {$ip}",
     "HTTP_REFERER: {$referer}",
     'Content-Type: application/json',
   ];
 
-    $data = [
+  $data = [
     'name' => $event_name,
     'page' => $locationUrl,
     "domain" => "ericexperiment.com",
     "screenWidth" => 1666,
   ];
 
-    if ($props !== false) {
-        $data['props'] = json_encode($props);
-    }
+  if ($props !== false) {
+    $data['props'] = json_encode($props);
+  }
 
-    $ch = curl_init($event_url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers);
+  $ch = curl_init($event_url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $additional_headers);
 
-    curl_exec($ch);
+  curl_exec($ch);
 }
 
-if (strpos($documentHost, 'localhost') === false) {
-    trackEvent('pageview');
-}
+
+
+trackEvent('pageview');
