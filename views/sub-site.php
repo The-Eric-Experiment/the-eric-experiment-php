1ab31fd7d14a726d1e3ef1345672a669e8d5cba7
Eric Mackrodt <ericmackrodt@gmail.com>
2024-08-26 05:48:45 +0000

    test subdomain again

--- views/sub-site.php
+++ views/sub-site.php
@@ -1,6 +1,30 @@
 <?php
+
+function getSubdomain() {
+    // Get the full host (e.g., subdomain.example.com)
+    $host = $_SERVER['HTTP_HOST'];
+
+    // Break the host into an array by dots
+    $parts = explode('.', $host);
+
+    // If there are more than 2 parts, then we have a subdomain
+    if (count($parts) > 2) {
+        // Remove the last two parts (domain and TLD)
+        array_pop($parts); // remove TLD
+        array_pop($parts); // remove domain
+
+        // The remaining parts are the subdomain
+        $subdomain = implode('.', $parts);
+    } else {
+        // If there are only two parts, there's no subdomain
+        $subdomain = '';
+    }
+
+    return $subdomain;
+}
+
 // Extract the subdomain from the query parameter
-$subdomain = isset($_GET['subdomain']) ? htmlspecialchars($_GET['subdomain']) : '';
+$subdomain = getSubdomain();
 
 // Extract the requested path from the URL
 $requestPath = isset($_SERVER['REQUEST_URI']) ? htmlspecialchars($_SERVER['REQUEST_URI']) : '';
